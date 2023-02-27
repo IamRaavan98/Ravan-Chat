64393f16ad43a2c0299d807df657773b1f0bff33
Rohan Agrawal <Rohanagrawal1798@gmail.com>
2023-02-27 17:18:55 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
+          <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
